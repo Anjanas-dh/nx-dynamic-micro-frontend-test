fd8bfe5084d2238f38ec109ad8e26b3783c83cb0
Anja van Hagen <anja.vanhagen@ns.nl>
2022-05-18 11:36:18 +0200

    Create workspace and add dynamic hosts and remotes projects

---
+++ apps/todo/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-entry',
+  template: `<dynamic-mfe-test-nx-welcome></dynamic-mfe-test-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
